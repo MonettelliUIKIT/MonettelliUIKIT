e6986a42188b0721525279f6d8e4cc6f6566f30a
Daniel A. Monetelli L <danielmonetelli@hotmail.com>
2019-12-14 13:25:04 -0500

    Add Template Architecture

---
+++ ProjectTemplates/XFMonettelli/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using $safeprojectname$.Views;
+
+namespace $safeprojectname$
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            //MainPage = new DemoPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
