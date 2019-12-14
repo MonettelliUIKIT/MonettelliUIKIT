71c8b4358a617d1ef37f447860705432460347c9
Daniel A. Monetelli L <danielmonetelli@hotmail.com>
2019-12-14 14:06:16 -0500

    Add Template Architecture finito

--- Monettelli.Forms.Templates/Monettelli.Forms.Templates/Properties/AssemblyInfo.cs
+++ Monettelli.Forms.Templates/Monettelli.Forms.Templates/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XFMonettelliVSIX")]
+[assembly: AssemblyTitle("Monettelli.Forms.Templates")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XFMonettelliVSIX")]
+[assembly: AssemblyProduct("Monettelli.Forms.Templates")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
