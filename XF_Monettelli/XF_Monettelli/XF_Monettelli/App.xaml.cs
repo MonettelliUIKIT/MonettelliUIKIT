7ad6740b52ad7822545fc6a22c92b6e2e13516d7
Daniel A. Monettelli L <danielmonetelli@hotmail.com>
2020-02-12 22:25:39 -0500

    Sorting the structure of the Project Template and adding the new icon.

---
+++ XF_Monettelli/XF_Monettelli/XF_Monettelli/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using XF_Monettelli.Views;
+
+namespace XF_Monettelli
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+            //MainPage = new DemoPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
