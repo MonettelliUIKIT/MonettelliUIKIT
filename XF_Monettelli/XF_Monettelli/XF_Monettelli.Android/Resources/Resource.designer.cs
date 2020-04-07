9c61e0c7daf85ae6ded101d1688e32b1fa26e802
Daniel A. Monettelli L <danielmonetelli@hotmail.com>
2020-04-06 21:34:15 -0500

    XF_Monettelli v2.0 Part6
    Arreglos finales.

--- XF_Monettelli/XF_Monettelli/XF_Monettelli.Android/Resources/Resource.designer.cs
+++ XF_Monettelli/XF_Monettelli/XF_Monettelli.Android/Resources/Resource.designer.cs
@@ -15,7 +15,7 @@ namespace XF_Monettelli.Android
 {
 	
 	
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
