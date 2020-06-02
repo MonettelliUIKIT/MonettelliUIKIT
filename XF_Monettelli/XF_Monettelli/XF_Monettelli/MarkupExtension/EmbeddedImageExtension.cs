3e0334a5a6fc4d4a92c2d07f2c2116000791d50d
Daniel A. Monettelli L <danielmonetelli@hotmail.com>
2020-06-02 11:56:01 -0500

    XF_Monettelli v3.0 Part1
    1.- Embedded fonts en Xamarin.Forms >4.6 (5/6).

--- XF_Monettelli/XF_Monettelli/XF_Monettelli/MarkupExtension/EmbeddedImageExtension.cs
+++ XF_Monettelli/XF_Monettelli/XF_Monettelli/MarkupExtension/EmbeddedImageExtension.cs
@@ -7,8 +7,6 @@ using Xamarin.Forms.Xaml;
 
 namespace XF_Monettelli.MarkupExtension
 {
-
-	// You exclude the 'Extension' suffix when using in Xaml markup
 	[ContentProperty("Source")]
 	public class EmbeddedImageExtension : IMarkupExtension
 	{
@@ -21,7 +19,6 @@ namespace XF_Monettelli.MarkupExtension
 				return null;
 			}
 
-			// Do your translation lookup here, using whatever method you require
 			var imageSource = ImageSource.FromResource(Source, typeof(EmbeddedImageExtension).GetTypeInfo().Assembly);
 
 			return imageSource;
